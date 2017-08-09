2ed4fbfa007297ff19c29c023bea63d60219f84e
jy19921020 <691096060@qq.com>
2017-08-09 10:45:08 +0800

    修改8.9.01

--- OCR_linkPage/build.sh
+++ OCR_linkPage/build.sh
@@ -2,19 +2,19 @@
 
 source /etc/profile
 
-MYIMAGE=127.0.0.1:8008/springboot/OCR_linkPage
+MYIMAGE=127.0.0.1:8008/springio/OCR_linkPage
 
 # 如果需要push，就取消注释
 #docker login 127.0.0.1:8008 -u 1063575596@qq.com -p xiao19921020
 
 # 暂停所有容器
-docker kill $(docker ps -aq)
+#docker kill $(docker ps -aq)
 
 # 删除所有容器
-docker rm $(docker ps -aq)
+#docker rm $(docker ps -aq)
 
 # 删除所有旧的镜像
-docker images | grep 127.0.0.1:8008/springio/OCR_linkPage | awk '{print $3}' | xargs docker rmi
+#docker images | grep 127.0.0.1:8008/springio/OCR_linkPage | awk '{print $3}' | xargs docker rmi
 
 # 构建jar包和镜像
 #mvn package -e -X docker:build -DskipTest
