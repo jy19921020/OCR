ccd62ea77147d9d5f27858d50b39c9ab53450f77
jy19921020 <691096060@qq.com>
2017-08-09 11:15:52 +0800

    创建镜像错误的修改 8.9.03

--- OCR_linkPage/build.sh
+++ OCR_linkPage/build.sh
@@ -17,12 +17,12 @@ MYIMAGE=127.0.0.1:8008/springio/OCR_linkPage
 #docker images | grep 127.0.0.1:8008/springio/OCR_linkPage | awk '{print $3}' | xargs docker rmi
 
 # 构建jar包和镜像
-#mvn package -e -X docker:build -DskipTest
-mvn package && docker build -t springio/OCR_linkPage
+mvn package -e -X docker:build -DskipTest
+#mvn package && docker build -t springio/OCR_linkPage
 
 # 运行容器
-#docker run -dp 8888:8008 --name OCR_linkPage ${MYIMAGE}
-docker run -d -p 8888:8008 --name sample-app springio/OCR_linkPage
+docker run -dp 8888:8008 --name OCR_linkPage ${MYIMAGE}
+
 
 # push image
 #docker push ${MYIMAGE}
